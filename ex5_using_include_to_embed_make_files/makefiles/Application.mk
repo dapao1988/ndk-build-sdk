e618daa6253c5866389db46dddc43e62f454a086
Wenbing.Wang <wenbing.wang@rokid.com>
2020-08-19 20:42:35 +0800

    Android.mk chain of responsibility

--- ex5_using_include_to_embed_make_files/makefiles/Application.mk
+++ ex5_using_include_to_embed_make_files/makefiles/Application.mk
@@ -1,6 +1,7 @@
 APP_OPTIM := debug
-APP_ABI := armeabi-v7a
-APP_STL := stlport_static 
+#APP_ABI := armeabi-v7a
+APP_ABI := all
+APP_STL := c++_static
 APP_CPPFLAGS := -frtti -fexceptions
 APP_PLATFORM := android-19
 APP_BUILD_SCRIPT := makefiles/Android.mk
